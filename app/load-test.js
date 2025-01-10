30ea2f77d0b94ff1d0c2ada0eb64048741a826f7
Mimi <fwfurtado@gmail.com>
2025-01-10 18:02:44 -0300

    get execution data

--- app/load-test.js
+++ app/load-test.js
@@ -1,4 +1,5 @@
 import http from 'k6/http';
+import exec from 'k6/execution';
 import { sleep } from 'k6';
 
 export const options = {
@@ -14,11 +15,45 @@ export default function() {
 }
 
 
-// export function handleSummary(data) {
-//   const med_latency = data.metrics.iteration_duration.values.med;
-//   const latency_message = `The median latency was ${med_latency}\n`;
+export function handleSummary(data) {
 
-//   return {
-//     stdout: latency_message,
-//   };
-// }
+  console.log(`Execution context
+
+    Instance info
+    -------------
+    Vus active: ${exec.instance.vusActive}
+    Iterations completed: ${exec.instance.iterationsCompleted}
+    Iterations interrupted:  ${exec.instance.iterationsInterrupted}
+    Iterations completed:  ${exec.instance.iterationsCompleted}
+    Iterations active:  ${exec.instance.vusActive}
+    Initialized vus:  ${exec.instance.vusInitialized}
+    Time passed from start of run(ms):  ${exec.instance.currentTestRunDuration}
+    
+    Scenario info
+    -------------
+    Name of the running scenario: ${exec.scenario.name}
+    Executor type: ${exec.scenario.executor}
+    Scenario start timestamp: ${exec.scenario.startTime}
+    Percenatage complete: ${exec.scenario.progress}
+    Iteration in instance: ${exec.scenario.iterationInInstance}
+    Iteration in test: ${exec.scenario.iterationInTest}
+    
+    Test info
+    ---------
+    All test options: ${exec.test.options}
+    
+    VU info
+    -------
+    Iteration id: ${exec.vu.iterationInInstance}
+    Iteration in scenario: ${exec.vu.iterationInScenario}
+    VU ID in instance: ${exec.vu.idInInstance}
+    VU ID in test: ${exec.vu.idInTest}
+    VU tags: ${exec.vu.tags}`);
+
+  const med_latency = data.metrics.iteration_duration.values.med;
+  const latency_message = `The median latency was ${med_latency}\n`;
+
+  return {
+    stdout: latency_message,
+  };
+}
