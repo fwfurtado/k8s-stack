d23492c2d3b78da0c8aec395381309125a5fbd4a
Mimi <fwfurtado@gmail.com>
2025-01-10 18:13:19 -0300

    get envs

--- app/load-test.js
+++ app/load-test.js
@@ -16,39 +16,7 @@ export default function() {
 
 
 export function handleSummary(data) {
-
-  console.log(`Execution context
-
-    Instance info
-    -------------
-    Vus active: ${exec.instance.vusActive}
-    Iterations completed: ${exec.instance.iterationsCompleted}
-    Iterations interrupted:  ${exec.instance.iterationsInterrupted}
-    Iterations completed:  ${exec.instance.iterationsCompleted}
-    Iterations active:  ${exec.instance.vusActive}
-    Initialized vus:  ${exec.instance.vusInitialized}
-    Time passed from start of run(ms):  ${exec.instance.currentTestRunDuration}
-    
-    Scenario info
-    -------------
-    Name of the running scenario: ${exec.scenario.name}
-    Executor type: ${exec.scenario.executor}
-    Scenario start timestamp: ${exec.scenario.startTime}
-    Percenatage complete: ${exec.scenario.progress}
-    Iteration in instance: ${exec.scenario.iterationInInstance}
-    Iteration in test: ${exec.scenario.iterationInTest}
-    
-    Test info
-    ---------
-    All test options: ${exec.test.options}
-    
-    VU info
-    -------
-    Iteration id: ${exec.vu.iterationInInstance}
-    Iteration in scenario: ${exec.vu.iterationInScenario}
-    VU ID in instance: ${exec.vu.idInInstance}
-    VU ID in test: ${exec.vu.idInTest}
-    VU tags: ${exec.vu.tags}`);
+  console.log('all env variables', JSON.stringify(__ENV));
 
   const med_latency = data.metrics.iteration_duration.values.med;
   const latency_message = `The median latency was ${med_latency}\n`;
